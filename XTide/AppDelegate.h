fa879efff68b53659db408089612e12ed9b3ffc1
Lee Ann Rucker <lrucker@macpro.local>
2016-04-11 12:28:08 -0700

    Initial Commit

---
+++ XTide/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  XTide
+//
+//  Created by Lee Ann Rucker on 4/11/16.
+//  Copyright © 2016 Lee Ann Rucker. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ XTide/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  XTide
+//
+//  Created by Lee Ann Rucker on 4/11/16.
+//  Copyright © 2016 Lee Ann Rucker. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
